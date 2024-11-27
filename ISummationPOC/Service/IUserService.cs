3791d0f850eb41ba3343b86c33a2e01d9cb6c62c
Aryan Singh <aryanrocks1703@gmail.com>
2024-11-27 15:32:33 +0530

    Master Test

--- ISummationPOC/Service/IUserService.cs
+++ ISummationPOC/Service/IUserService.cs
@@ -5,12 +5,11 @@ using ISummationPOC.Repository;
 namespace ISummationPOC.Service
 {
     public interface IUserService  :IRepository<User>
-    {
-       
+    {       
         Task<IEnumerable<UserViewModel>> GetAllUserAsync();
         Task<User> GetUserByIdAsync(int id);       
         Task <User> CreateUserAsync(User user, IFormFile ProfileImage);        
-        Task <User> UpdateUser(User user, IFormFile ProfileImage);
+        Task <User> UpdateUser(User user ,IFormFile ProfileImage);
         Task<int> DeleteUserAsync(int id);
     }
 }
